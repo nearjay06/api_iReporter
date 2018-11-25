e9c586ffc7f70a72ea0a0aeffce7aa6a67c50f15
nearjay06 <nearjoan2018@gmail.com>
2018-11-25 22:56:03 +0300

    modify tests endpoints file
    [#162179297]

---
+++ tests/test_endpoints.py
@@ -0,0 +1,20 @@
+import unittest
+from api.routes import app
+import json
+
+class TestEndpoints(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+
+
+
+
+
+
+
+
+
+
+
+if __name__== '__main__':
+ unittest.main()
