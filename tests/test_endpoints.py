fbc6bef403741821df842d9a872002af7cd65274
nearjay06 <nearjoan2018@gmail.com>
2018-11-25 23:38:49 +0300

    write tests fro endpoints
    [#162179297]

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -1,10 +1,42 @@
 import unittest
-from api.routes import app
+from api.routes import endpoints
+from api.models.incident import Incidents
+from api.models.user import Users
 import json
 
 class TestEndpoints(unittest.TestCase):
     def setUp(self):
-        self.app = app.test_client()
+        self.api = endpoints.test_client()
+
+    def test_get_all_redflag_records(self):
+        response = self.app.get('api/v1/redflags')
+        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.content_type,'application/json')
+
+    def test_get_redflag_records(self):
+        response = self.app.get('/api/v1/redflags')
+        self.assertNotIsInstance('email',Incidents,"message")
+
+    def test_post_redflag_records(self):
+        response = self.app.post('/api/v1/redflags')
+        self.assertTrue({'incident id must not be a string','message'},True)
+
+    def test_get_specific_redflag_with_id(self):
+        response = self.app.get('/api/v1/redflags/1')
+        self.assertNotIn('status',Users,"message")
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
